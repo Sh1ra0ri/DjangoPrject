d80d68d761ac85c44b807e282dc46e5db3e8df0b
Sh1ra0ri <xxnon.7@gmail.com>
2025-11-11 11:08:13 +0300

    .

--- catalog/models.py
+++ catalog/models.py
@@ -21,12 +21,11 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
-
 class Product(models.Model):
     name = models.CharField(max_length=100,verbose_name='Наименование',help_text='Введите наименование продукта')
     description = models.CharField(max_length=200,verbose_name='Описание',help_text='Введите описание продукта')
     image = models.ImageField(upload_to='products/photo',blank=True,null=True,verbose_name='Изображение',help_text='Загрузите фото продукта')
-    category = models.ForeignKey(Category,on_delete=models.CASCADE,verbose_name='Категория',help_text='Выберите категорию продукта')
+    category = models.ForeignKey(Category,on_delete=models.SET_NULL,verbose_name='Категория',help_text='Выберите категорию продукта',null=True, blank=True)
     price = models.DecimalField(max_digits=10,decimal_places=2,verbose_name='Цена за покупку',help_text='Введите цену в рублях')
     created_at = models.DateTimeField(auto_now_add=True,verbose_name='Дата создания')
     updated_at = models.DateTimeField(auto_now=True,verbose_name='Дата последнего изменения')
